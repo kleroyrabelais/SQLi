4efb21337983425f734c949d5d4e92d20af5f1db
unknown <le_roy@220056slocal>
2025-01-24 09:19:31 +0100

    tous les fichier SQLi prêt a l'emploi

---
+++ connexion.php
@@ -0,0 +1,25 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<?php
+try {
+
+$dns = 'mysql:host=localhost;dbname=exemple'; // dbname : nom de la base
+
+$utilisateur = 'root'; // root sur vos postes
+
+$motDePasse = ''; // pas de mot de passe sur vos postes
+
+$connexion = new PDO( $dns, $utilisateur, $motDePasse );
+
+} catch (Exception $e) {
+
+echo "Connexion à MySQL impossible : ", $e->getMessage();
+
+die();
+
+}
+
+?>
