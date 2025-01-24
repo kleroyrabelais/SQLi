942608f2dced8faac0605a6dd34737df75227525
kleroyrabelais <kleroypro22@gmail.com>
2025-01-24 09:29:07 +0100

    ajout d'un authentification protégé par patterne

---
+++ c_authentification_protéger.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<?php
+
+if (!isset($_POST['btnSeConnecter'])) { /* L'entrée btnSeConnecter est vide = le formulaire n'a pas été posté, on affiche le formulaire */
+
+    // GENERATION FORMULAIRE //
+
+    echo '
+
+    <form action="" method = "post" ">
+
+        Mel: <input name="mel" type="text" size ="30" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"">
+
+        Mot de passe: <input name="mot_de_passe" type="text" size ="30" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" ">
+
+        <input type="submit" name="btnSeConnecter"  value="Se connecter">
+
+    </form>';
+
+} else
+
+/* L'utilisateur a cliqué sur Se connecter, l'entrée btnSeConnecter <> vide, on traite le formulaire */
+
+{
+
+    // TRAITEMENT FORMULAIRE //
+
+// On se connecte
+
+    require_once 'connexion.php';
+
+    $mel = $_POST['mel'];
+
+    $mot_de_passe = $_POST['mot_de_passe'];
+
+ 
+
+    $requete = "SELECT numero FROM utilisateur WHERE mel='" . $mel . "' AND mot_de_passe = '" . $mot_de_passe . "'";
+
+    echo "Requête générée : ".$requete . "</BR>"; // affichage requête générée, pour information
+
+ 
+
+// Envoi de la requête vers MySQL
+
+    $select = $connexion->query($requete);
+
+// Les résultats retournés par la requête seront traités en 'mode' objet
+
+    $select->setFetchMode(PDO::FETCH_OBJ);
+
+// Traitement d'un seul résultat
+
+    $enregistrement = $select->fetch();
+
+    if ($enregistrement) { // si $enregistrement n'est pas vide = on a trouvé quelque chose -> on est connecté
+
+        echo '<h1>Connexion réussie !</h1>';
+
+    } else { // La requête n'a pas retournée de résultat, on a pas trouvé de ligne correspondant au mel et mot de passe
+
+        echo "<h1>Echec à la connexion.</h1>";
+
+    }
+
+}
+
+?>
+
+</body>
+
+</html>
