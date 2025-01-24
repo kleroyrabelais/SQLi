c544d6348c150b6302b59b8e0e286059d1afa325
kleroyrabelais <kleroypro22@gmail.com>
2025-01-24 10:03:03 +0100

    fix du fichier auth protéger aprés carac d'échappement

--- c_authentification_protéger.php
+++ c_authentification_protéger.php
@@ -60,10 +60,6 @@ if (!isset($_POST['btnSeConnecter'])) { /* L'entrée btnSeConnecter est vide = l
     if ($enregistrement) { // si $enregistrement n'est pas vide = on a trouvé quelque chose -> on est connecté
 
         echo '<h1>Connexion réussie !</h1>';
-        // on regarde d'alembert et on ajoute un caractère d'échappement pour le rendre sûre a la base de donnée
-$nom = "d'Alembert";
-echo $nom. " L'insertion de ce nom n'est pas sûre. <br>";
-echo addslashes($nom). " L'ajout du caractère d'échappement rend l'insertion sûre.";
 
     } else { // La requête n'a pas retournée de résultat, on a pas trouvé de ligne correspondant au mel et mot de passe
 
